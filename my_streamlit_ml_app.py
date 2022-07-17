50bcf24554d5df17aab07aa3e30c4bf8f143329e
Hamid Bekamiri <hamid.bekam@gmail.com>
2022-07-17 14:18:12 +0200

    Update my_streamlit_ml_app.py

--- my_streamlit_ml_app.py
+++ my_streamlit_ml_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from sentence_transformers import SentenceTransformer, util
+model = SentenceTransformer('AI-Growth-Lab/PatentSBERTa')
 
 # Two lists of sentences
 sentences1 = st.text_input('Insert sentences 1:')
