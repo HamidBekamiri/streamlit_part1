dc03f0191118b67143a5558d0afa96c9e3edbb2f
Hamid Bekamiri <hamid.bekam@gmail.com>
2022-07-17 14:07:25 +0200

    Add files via upload

---
+++ my_streamlit_ml_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Two lists of sentences
+sentences1 = st.text_input('Insert sentences 1:')
+
+
+sentences2 = st.text_input('Insert sentences 2:')
+
+
+
+
+st.write('sentences1 is: ', sentences1)
+st.write('sentences2 is: ', sentences2)
+
