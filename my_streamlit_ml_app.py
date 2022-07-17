5848c408b6d90d6fcf773806fd20e535f304edfb
Hamid Bekamiri <hamid.bekam@gmail.com>
2022-07-17 15:17:21 +0200

    Update my_streamlit_ml_app.py

--- my_streamlit_ml_app.py
+++ my_streamlit_ml_app.py
@@ -22,6 +22,5 @@ if st.button('Submit'):
   cosine_scores = util.cos_sim(embeddings1, embeddings2)
 
   #Output the pairs with their score
-  st.write(sentences1, sentences2, cosine_scores)
-  st.write(sentences2)
-  st.write(cosine_scores)
+  st.write(sentences1, sentences2, cosine_scores[0])
+
